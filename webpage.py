417ab05fbf85c4d9157daa1805db4bdad175a777
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2026-01-04 01:49:20 +0530

    Refactor webpage.py for improved functionality

--- webpage.py
+++ webpage.py
@@ -22,7 +22,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 # Initialize Pinecone instance
-pc = Pinecone(api_key=os.getenv(PINECONE_API_KEY))
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
 # Check if the index exists, create if not
 if "medii" not in pc.list_indexes().names():
@@ -99,6 +99,7 @@ if user_input:
     chatbot_response = get_chatbot_response(user_input)
     st.session_state["messages"].append({"text": user_input, "time": timestamp})
     st.session_state["messages"].append({"text": chatbot_response, "time": timestamp})
+
 
 
 
